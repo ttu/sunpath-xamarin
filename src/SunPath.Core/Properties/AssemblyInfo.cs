46cb604bf682e3ca98ad73833b17742bda7f25e5
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2014-05-15 19:10:49 +0300

    Core as Portable library

--- src/SunPath.Core/Properties/AssemblyInfo.cs
+++ src/SunPath.Core/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,19 +9,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SunPath.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SunPath.Core")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9e8a2e7c-225a-40ca-b88f-2c917748327b")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
--- src/SunPath.WP/MainPage.xaml.cs
+++ src/SunPath.WP/MainPage.xaml.cs
@@ -1,16 +1,12 @@
 ﻿using Microsoft.Devices;
 using Microsoft.Phone.Controls;
 using Microsoft.Xna.Framework.Media;
-using System;
-using System.IO;
-using System.IO.IsolatedStorage;
 using System.Windows;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace SunPath.WP
 {
-    // http://msdn.microsoft.com/en-us/library/windowsphone/develop/hh202956(v=vs.105).aspx#BKMK_CreatingTheCameraUI
-
     public partial class MainPage : PhoneApplicationPage
     {
         // Variables
@@ -18,43 +14,31 @@ namespace SunPath.WP
 
         private PhotoCamera cam;
         private MediaLibrary library = new MediaLibrary();
+        private PointCollection _points = new PointCollection();
 
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+
+            var myPolygon = new Polygon();
+            myPolygon.Stroke = new SolidColorBrush(Colors.White);
+            myPolygon.StrokeThickness = 2;
+            myPolygon.HorizontalAlignment = HorizontalAlignment.Left;
+            myPolygon.VerticalAlignment = VerticalAlignment.Center;
+
+            myPolygon.Points = _points;
+
+            paintSurface.Children.Add(myPolygon);
         }
 
         //Code for initialization, capture completed, image availability events; also setting the source for the viewfinder.
         protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
         {
             // Check to see if the camera is available on the phone.
-            if ((PhotoCamera.IsCameraTypeSupported(CameraType.Primary) == true) ||
-                 (PhotoCamera.IsCameraTypeSupported(CameraType.FrontFacing) == true))
+            if (PhotoCamera.IsCameraTypeSupported(CameraType.Primary) == true)
             {
-                // Initialize the camera, when available.
-                if (PhotoCamera.IsCameraTypeSupported(CameraType.FrontFacing))
-                {
-                    // Use front-facing camera if available.
-                    cam = new Microsoft.Devices.PhotoCamera(CameraType.FrontFacing);
-                }
-                else
-                {
-                    // Otherwise, use standard camera on back of phone.
-                    cam = new Microsoft.Devices.PhotoCamera(CameraType.Primary);
-                }
-
-                // Event is fired when the PhotoCamera object has been initialized.
-                cam.Initialized += new EventHandler<Microsoft.Devices.CameraOperationCompletedEventArgs>(cam_Initialized);
-
-                // Event is fired when the capture sequence is complete.
-                cam.CaptureCompleted += new EventHandler<CameraOperationCompletedEventArgs>(cam_CaptureCompleted);
-
-                // Event is fired when the capture sequence is complete and an image is available.
-                cam.CaptureImageAvailable += new EventHandler<Microsoft.Devices.ContentReadyEventArgs>(cam_CaptureImageAvailable);
-
-                // Event is fired when the capture sequence is complete and a thumbnail image is available.
-                cam.CaptureThumbnailAvailable += new EventHandler<ContentReadyEventArgs>(cam_CaptureThumbnailAvailable);
+                cam = new Microsoft.Devices.PhotoCamera(CameraType.Primary);
 
                 //Set the VideoBrush source to the camera.
                 viewfinderBrush.SetSource(cam);
@@ -79,25 +63,6 @@ namespace SunPath.WP
             {
                 // Dispose camera to minimize power consumption and to expedite shutdown.
                 cam.Dispose();
-
-                // Release memory, ensure garbage collection.
-                cam.Initialized -= cam_Initialized;
-                cam.CaptureCompleted -= cam_CaptureCompleted;
-                cam.CaptureImageAvailable -= cam_CaptureImageAvailable;
-                cam.CaptureThumbnailAvailable -= cam_CaptureThumbnailAvailable;
-            }
-        }
-
-        // Update the UI if initialization succeeds.
-        private void cam_Initialized(object sender, CameraOperationCompletedEventArgs e)
-        {
-            if (e.Succeeded)
-            {
-                this.Dispatcher.BeginInvoke(() =>
-                {
-                    // Write message.
-                    txtDebug.Text = "Camera initialized.";
-                });
             }
         }
 
@@ -108,9 +73,6 @@ namespace SunPath.WP
             {
                 // LandscapeRight rotation when camera is on back of phone.
                 int landscapeRightRotation = 180;
-
-                // Change LandscapeRight rotation for front-facing camera.
-                if (cam.CameraType == CameraType.FrontFacing) landscapeRightRotation = -180;
 
                 // Rotate video brush from camera.
                 if (e.Orientation == PageOrientation.LandscapeRight)
@@ -130,128 +92,15 @@ namespace SunPath.WP
             base.OnOrientationChanged(e);
         }
 
-        // Informs when full resolution photo has been taken, saves to local media library and the local folder.
-        private void cam_CaptureImageAvailable(object sender, ContentReadyEventArgs e)
-        {
-            string fileName = savedCounter + ".jpg";
-
-            try
-            {   // Write message to the UI thread.
-                Deployment.Current.Dispatcher.BeginInvoke(() =>
-                {
-                    txtDebug.Text = "Captured image available, saving photo.";
-                });
-
-                // Save photo to the media library camera roll.
-                library.SavePictureToCameraRoll(fileName, e.ImageStream);
-
-                // Write message to the UI thread.
-                Deployment.Current.Dispatcher.BeginInvoke(() =>
-                {
-                    txtDebug.Text = "Photo has been saved to camera roll.";
-                });
-
-                // Set the position of the stream back to start
-                e.ImageStream.Seek(0, SeekOrigin.Begin);
-
-                // Save photo as JPEG to the local folder.
-                using (IsolatedStorageFile isStore = IsolatedStorageFile.GetUserStoreForApplication())
-                {
-                    using (IsolatedStorageFileStream targetStream = isStore.OpenFile(fileName, FileMode.Create, FileAccess.Write))
-                    {
-                        // Initialize the buffer for 4KB disk pages.
-                        byte[] readBuffer = new byte[4096];
-                        int bytesRead = -1;
-
-                        // Copy the image to the local folder.
-                        while ((bytesRead = e.ImageStream.Read(readBuffer, 0, readBuffer.Length)) > 0)
-                        {
-                            targetStream.Write(readBuffer, 0, bytesRead);
-                        }
-                    }
-                }
-
-                // Write message to the UI thread.
-                Deployment.Current.Dispatcher.BeginInvoke(delegate()
-                {
-                    txtDebug.Text = "Photo has been saved to the local folder.";
-                });
-            }
-            finally
-            {
-                // Close image stream
-                e.ImageStream.Close();
-            }
-        }
-
-        // Informs when thumbnail photo has been taken, saves to the local folder
-        // User will select this image in the Photos Hub to bring up the full-resolution.
-        public void cam_CaptureThumbnailAvailable(object sender, ContentReadyEventArgs e)
-        {
-            string fileName = savedCounter + "_th.jpg";
-
-            try
-            {
-                // Write message to UI thread.
-                Deployment.Current.Dispatcher.BeginInvoke(delegate()
-                {
-                    txtDebug.Text = "Captured image available, saving thumbnail.";
-                });
-
-                // Save thumbnail as JPEG to the local folder.
-                using (IsolatedStorageFile isStore = IsolatedStorageFile.GetUserStoreForApplication())
-                {
-                    using (IsolatedStorageFileStream targetStream = isStore.OpenFile(fileName, FileMode.Create, FileAccess.Write))
-                    {
-                        // Initialize the buffer for 4KB disk pages.
-                        byte[] readBuffer = new byte[4096];
-                        int bytesRead = -1;
-
-                        // Copy the thumbnail to the local folder.
-                        while ((bytesRead = e.ImageStream.Read(readBuffer, 0, readBuffer.Length)) > 0)
-                        {
-                            targetStream.Write(readBuffer, 0, bytesRead);
-                        }
-                    }
-                }
-
-                // Write message to UI thread.
-                Deployment.Current.Dispatcher.BeginInvoke(delegate()
-                {
-                    txtDebug.Text = "Thumbnail has been saved to the local folder.";
-                });
-            }
-            finally
-            {
-                // Close image stream
-                e.ImageStream.Close();
-            }
-        }
-
         private void ShutterButton_Click(object sender, RoutedEventArgs e)
         {
-            if (cam != null)
-            {
-                try
-                {
-                    // Start image capture.
-                    cam.CaptureImage();
-                }
-                catch (Exception ex)
-                {
-                    this.Dispatcher.BeginInvoke(delegate()
-                    {
-                        // Cannot capture an image until the previous capture has completed.
-                        txtDebug.Text = ex.Message;
-                    });
-                }
-            }
-        }
+            _points.Clear();
 
-        private void cam_CaptureCompleted(object sender, CameraOperationCompletedEventArgs e)
-        {
-            // Increments the savedCounter variable used for generating JPEG file names.
-            savedCounter++;
+            for (float x = 50; x < 1000; x += 5)
+            {
+                var p = new Point(x, x);
+                _points.Add(p);
+            }
         }
     }
 }
